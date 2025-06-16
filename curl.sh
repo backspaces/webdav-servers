4d717d897f19afb1d5b029effadfebef8bac986f
Owen Densmore <owen@backspaces.net>
2025-06-16 13:06:38 -0600

    Cleanup repo. Add local denocli webdav server

--- curl.sh
+++ curl.sh
@@ -10,7 +10,7 @@ root='https://webdav-server.glitch.me'      # Glitch
 root='https://backspaces.net/webdav-one'    # cPanel
 root='https://backspaces.net/webdav-two'    # cPanel
 root='https://webdav-server.deno.dev'       # Deno Deploy
-root='https://webdav-servers.onrender.com'  # Render
+root='http://3.137.171.107:8888'            # AWS
 
 root='https://backspaces.net/echo-server'   # cPanel
 
